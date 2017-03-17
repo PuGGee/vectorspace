0614125ade7965fc2570b93546f7c127e5856158
Lewis Wrigley <lewis.wrigley@gmail.com>
2017-03-17 23:56:32 +0000

    Refactor PlayerData to use singletone pattern
      So we can save and load the players data more easily

--- Assets/Scripts/Storage/PlayerData.cs
+++ Assets/Scripts/Storage/PlayerData.cs
@@ -3,47 +3,57 @@ using System.Collections;
 
 public class PlayerData {
 
-  private static int credits_data;
-  private static Transform ship_data;
-  private static Transform[] equipment_data;
-  private static ArrayList inventory_data;
+  public int _credits { get; set; }
+  public Transform _ship { get; set; }
+  public Transform[] _equipment { get; set; }
+  public ArrayList _inventory { get; set; }
+
+  private static PlayerData _instance;
+  private static PlayerData instance {
+    get {
+      if (_instance == null) {
+        _instance = new PlayerData();
+      }
+      return _instance;
+    }
+  }
 
   public static int credits {
     get {
-      return credits_data;
+      return instance._credits;
     }
     set {
-      credits_data = value;
+      instance._credits = value;
     }
   }
 
   public static Transform ship {
     get {
-      return ship_data;
+      return instance._ship;
     }
     set {
-      ship_data = value;
+      instance._ship = value;
       empty_equipment();
-      equipment_data = new Transform[ShipHelper.hardpoints(ship_data).Length];
+      instance._equipment = new Transform[ShipHelper.hardpoints(ship).Length];
     }
   }
 
   public static ArrayList inventory {
     get {
-      if (inventory_data == null) {
-        inventory_data = new ArrayList();
+      if (instance._inventory == null) {
+        instance._inventory = new ArrayList();
       }
-      return inventory_data;
+      return instance._inventory;
     }
   }
 
   public static Blueprint blueprint {
     get {
       Blueprint player_blueprint = new Blueprint();
-      player_blueprint.ship_prefab = ship_data;
+      player_blueprint.ship_prefab = ship;
 
-      for (int i = 0; i < equipment_data.Length; i++) {
-        player_blueprint.add_equipment(i, equipment_data[i]);
+      for (int i = 0; i < instance._equipment.Length; i++) {
+        player_blueprint.add_equipment(i, instance._equipment[i]);
       }
 
       return player_blueprint;
@@ -60,24 +70,24 @@ public class PlayerData {
 
   public static void add_equipment(int index, Transform item) {
     empty_hardpoint(index);
-    equipment_data[index] = item;
+    instance._equipment[index] = item;
     inventory.Remove(item);
   }
 
   public static Transform equipment_at(int index) {
-    return equipment_data[index];
+    return instance._equipment[index];
   }
 
   public static void empty_hardpoint(int index) {
     if (equipment_at(index) != null) {
       inventory.Add(equipment_at(index));
-      equipment_data[index] = null;
+      instance._equipment[index] = null;
     }
   }
 
   private static void empty_equipment() {
-    if (equipment_data == null) return;
-    for (int i = 0; i < equipment_data.Length; i++) {
+    if (instance._equipment == null) return;
+    for (int i = 0; i < instance._equipment.Length; i++) {
       empty_hardpoint(i);
     }
   }
