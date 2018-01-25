cf3ce54a2a0b02d1957650fa8a342d9be15ab359
Puggee <lewis.wrigley@gmail.com>
2018-01-25 23:13:40 +0000

    Make camera more dynamic
      The camera now follows the player less rigidly, so it
      appears more fluid

--- Assets/Scripts/Controllers/CameraControl.cs
+++ Assets/Scripts/Controllers/CameraControl.cs
@@ -14,10 +14,28 @@ public class CameraControl : MonoBehaviour {
   }
 
   void Update() {
-    var direction_vector = GlobalObjects.player.transform.rotation * Vector2.up * distance;
-    transform.position = GlobalObjects.player.transform.position + displacement + direction_vector;
-    transform.rotation = GlobalObjects.player.transform.rotation;
+    update_position();
+    update_rotation();
     set_scale(Scale.value);
+  }
+
+  private void update_position() {
+    var direction_vector = GlobalObjects.player.transform.rotation * Vector2.up * distance;
+    var target_position = GlobalObjects.player.transform.position + displacement + direction_vector;
+    var distance_from_target_position = target_position - transform.position;
+    transform.position += distance_from_target_position / 10f;
+  }
+
+  private void update_rotation() {
+    var rotational_displacement_euler = GlobalObjects.player.transform.rotation.eulerAngles - transform.rotation.eulerAngles;
+    var rotation = transform.rotation;
+    if (rotational_displacement_euler.z > 180) {
+      rotational_displacement_euler.z -= 360;
+    } else if (rotational_displacement_euler.z < -180) {
+      rotational_displacement_euler.z += 360;
+    }
+    rotation.eulerAngles = rotation.eulerAngles + (rotational_displacement_euler / 10f);
+    transform.rotation = rotation;
   }
 
   private void set_scale(int scale) {
